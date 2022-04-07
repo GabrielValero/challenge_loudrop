ac7c75942ff626ff97e7aa4b883732e8d181923e
Gabriel Valero <gabriel.131280@gmail.com>
2022-04-07 16:27:34 -0400

    Se agregaron imagenes e iconos y productos en el json

---
+++ src/config/products.js
@@ -0,0 +1,67 @@
+const products = [
+  {
+    id: 1,
+    name:'Reloj',
+    price: 5,
+    img: '/assets/reloj.png',
+    category: ["joyería", "accesorio"],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+  },
+  {
+    id: 2,
+    name:'Microfono Blue',
+    price: 80,
+    img: '/assets/BlueMicrofone.png',
+    category: ["electrónica", "audio", "micrófono"],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+  },
+  {
+    id: 3,
+    name:'Colgante de shingeki no Kyojin',
+    price: 15,
+    img: '/assets/Colgante.png',
+    category: ["joyería", "accesorio"],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+  },
+  {
+    id: 4,
+    name:'Licuadora Black & Decker',
+    price: 45,
+    img: '/assets/Licuadora.png',
+    category: ["electrónica","licuadoras"],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+  },
+  {
+    id: 5,
+    name:'Nintendo Switch',
+    price: 250,
+    img: '/assets/NintendoSwitch.png',
+    category: ["electrónica","games"],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+  },
+  {
+    id: 6,
+    name:'Controles de Ps4',
+    price: 25,
+    img: '/assets/PsControls.png',
+    category: ["electrónica","games"],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+  },
+  {
+    id: 7,
+    name:'Zapato Nike',
+    price: 60,
+    img: '/assets/Shoe.png',
+    category: ["ropa","zapatos"],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+  },
+  {
+    id: 8,
+    name:'Video juego para Ps4 The Last Of Us',
+    price: 30,
+    img: '/assets/The_Last_Of_Us.png',
+    category: ["ropa","zapatos"],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+  },
+]
+export default products
